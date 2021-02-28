1b2627615835ecea776c86be6f12feb6177197ed
Adam Macken <mackenadam@gmail.com>
2021-02-28 15:00:59 +1300

    Switch/if statement practice

--- SwitchStatements/Program.cs
+++ SwitchStatements/Program.cs
@@ -6,8 +6,35 @@ namespace SwitchStatements
     {
         static void Main(string[] args)
         {
-            // Switch statements in C#
+            // Switch and case statements in C#
+            int temperature = 25;
 
+            switch(temperature)
+            {
+                case 15:
+                    Console.WriteLine("Not too hot, not too cold");
+                    break;
+                case 25:
+                    Console.WriteLine("Warm as!");
+                    break;
+                default:
+                    Console.WriteLine("How warm is it then?");
+                    break;
+            }
+
+            // Reproduce above as an if statement
+            if (temperature == 25)
+            {
+                Console.WriteLine("Warm as");
+            } 
+            else if (temperature == 15)
+            {
+                Console.WriteLine("Not too hot, not too cold.");
+            } 
+            else
+            {
+                Console.WriteLine("How warm is it then?");
+            }
         }
     }
 }
