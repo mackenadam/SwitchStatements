3d6935fa15aee54b57ce98802e6aa0dc0a4e2ea0
Adam Macken <mackenadam@gmail.com>
2021-02-28 14:54:29 +1300

    Add project files.

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Switch statements in C#
+
+        }
+    }
+}
